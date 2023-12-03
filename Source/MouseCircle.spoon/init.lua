43ce92845360933692b16a3d1e20b05fbded165a
Matthew Fallshaw <m@fallshaw.me>
2023-12-04 09:33:19 +1100

    Update MouseCircle to show a contracting circle

--- Source/MouseCircle.spoon/init.lua
+++ Source/MouseCircle.spoon/init.lua
@@ -9,7 +9,7 @@ obj.__index = obj
 
 -- Metadata
 obj.name = "MouseCircle"
-obj.version = "1.0"
+obj.version = "1.1"  -- Updated version number
 obj.author = "Chris Jones <cmsj@tenshu.net>"
 obj.homepage = "https://github.com/Hammerspoon/Spoons"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
@@ -18,12 +18,12 @@ obj.circle = nil
 obj.timer = nil
 obj.hotkey = nil
 
-obj.dia = 80
+obj.dia = 80  -- Diameter of the smallest circle
 
---- MouseCircle.color
+--- MouseCircle.colour
 --- Variable
---- An `hs.drawing.color` table defining the colour of the circle. Defaults to red.
-obj.color = nil
+--- An hs.drawing.colour table defining the colour of the circle. Defaults to red.
+obj.colour = {["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1}
 
 --- MouseCircle:bindHotkeys(mapping)
 --- Method
@@ -33,9 +33,14 @@ obj.color = nil
 ---  * mapping - A table containing hotkey modifier/key details for the following items:
 ---   * show - This will cause the mouse circle to be drawn
 function obj:bindHotkeys(mapping)
-    if (self.hotkey) then
+    if type(mapping) ~= "table" or not mapping["show"] then
+        error("Invalid mapping")
+    end
+
+    if self.hotkey then
         self.hotkey:delete()
     end
+
     local showMods = mapping["show"][1]
     local showKey = mapping["show"][2]
     self.hotkey = hs.hotkey.bind(showMods, showKey, function() self:show() end)
@@ -53,54 +58,74 @@ end
 --- Returns:
 ---  * None
 function obj:show()
-    local circle = self.circle
-    local timer = self.timer
-    local dia = obj.dia
-
-    if circle then
-        circle:hide(0.5)
-        if timer then
-            timer:stop()
-        end
+    -- Stop and delete existing circle and timer if they exist
+    if self.circle then
+        self.circle:delete()
+        self.circle = nil
+    end
+    if self.timer then
+        self.timer:stop()
+        self.timer = nil
     end
 
-    mousepoint = hs.mouse.absolutePosition()
+    -- Get the current mouse position
+    local mousepoint = hs.mouse.absolutePosition()
 
-    local color = nil
-    if (self.color) then
-        color = self.color
-    else
-        color = {["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1}
-    end
-    circle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-dia/2, mousepoint.y-dia/2, dia, dia))
-    circle:setStrokeColor(color)
-    circle:setFill(false)
-    circle:setStrokeWidth(5)
-    circle:bringToFront(true)
+    -- Define the initial diameter and step for reducing the size
+    local scale = 10
+    local initialDia = self.dia * scale
 
-    self.circle = circle
+    -- Define the stroke width
+    local strokeWidth = 5
+    local halfStroke = strokeWidth / 2
 
-    self.pulsetimer = hs.timer.doEvery(0.4, function()
-        if self.circle then
-            self.circle:show(0.1)
-            hs.timer.doAfter(0.15, function()
-                if self.circle then self.circle:hide(0.1) end
-            end)
-        end
-    end)
+    -- Adjust canvas size to account for stroke width
+    self.circle = hs.canvas.new({
+        x = mousepoint.x - initialDia / 2 - halfStroke,
+        y = mousepoint.y - initialDia / 2 - halfStroke,
+        h = initialDia + strokeWidth,
+        w = initialDia + strokeWidth
+    })
+    self.circle:appendElements({
+        type = "circle",
+        action = "stroke",
+        strokeWidth = strokeWidth,
+        strokeColour = obj.colour,
+        padding = halfStroke
+    }):level(hs.canvas.windowLevels.cursor)
 
-    self.timer = hs.timer.doAfter(3, function()
-        if self.circle then self.circle:hide(0.1) end
-        if self.pulsetimer then self.pulsetimer:stop() end
-        hs.timer.doAfter(0.15, function()
-            if self.circle then
+    -- Additional properties for the sinusoidal animation
+    local animationDuration = 2  -- Total duration of the animation in seconds
+    local startTime = hs.timer.secondsSinceEpoch()
+
+    local function updateCircleSize()
+        local currentTime = hs.timer.secondsSinceEpoch()
+        local elapsedTime = currentTime - startTime
+
+        if elapsedTime < animationDuration then
+            local sineValue = math.cos(math.pi * elapsedTime / animationDuration)
+            local dia = self.dia + sineValue * (self.dia * (scale - 1)) -- oscillate between dia and 1.5 * dia
+
+            -- Update frame and show immediately
+            self.circle:frame({
+                x = mousepoint.x - dia / 2 - halfStroke,
+                y = mousepoint.y - dia / 2 - halfStroke,
+                h = dia + strokeWidth,
+                w = dia + strokeWidth
+            })
+            self.circle[1].frame = { x = halfStroke, y = halfStroke, h = dia, w = dia }
+            self.circle:show()
+        else
+            self.timer:stop()
+            hs.timer.doAfter(0.1, function()
                 self.circle:delete()
                 self.circle = nil
-            end
-            if self.pulsetimer then self.pulsetimer = nil end
-            if self.timer then self.timer = nil end
-        end)
-    end)
+            end)
+        end
+    end
+
+    -- Adjust the timer interval for smoother animation
+    self.timer = hs.timer.doEvery(0.05, updateCircleSize)
 
     return self
 end
